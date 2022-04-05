becc82a1f46270cc80d762e6d26fa4c01b49c6b1
Levi Milan <lwmilan@mtu.edu>
2022-04-05 04:01:50 -0400

    Finished Creating Shared Arrays

--- main.c
+++ main.c
@@ -1,4 +1,20 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h> //For wait() probably can delete later
+
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <sys/types.h>
+
+// Use shmget() to allocate a shared memory.
+// Use shmat() to attach a shared memory to an address space.
+// Use shmdt() to detach a shared memory from an address space.
+// Use shmctl() to deallocate a shared memory.
+// #include  <sys/types.h>
+// #include  <sys/ipc.h>
+// #include  <sys/shm.h>
+
+// Use execvp() to call other programs
 
 int main(int argc, char *argv[]) {
   int qNum, xNum, yNum;
@@ -17,10 +33,181 @@ int main(int argc, char *argv[]) {
     fscanf(fptr, "%d", &inputNum);
     qArr[i] = inputNum;
   }
+  // For Loading xsort array
+  fscanf(fptr, "%d", &xNum);
+  int xArr[xNum];
+  for (int i = 0; i < xNum; i++) {
+    fscanf(fptr, "%d", &inputNum);
+    xArr[i] = inputNum;
+  }
+  // For Loading xsort array
+  fscanf(fptr, "%d", &yNum);
+  int yArr[yNum];
+  for (int i = 0; i < yNum; i++) {
+    fscanf(fptr, "%d", &inputNum);
+    yArr[i] = inputNum;
+  }
+  fclose(fptr);
+
+  printf("qNum: %d\txNum: %d\tyNum: %d\n", qNum, xNum, yNum);
+  // Just For Printing the qArr
   for (int i = 0; i < qNum; i++) {
-    printf("%d, ", qArr[i]);
+    if (i != (qNum - 1)) {
+      printf("%d, ", qArr[i]);
+    } else {
+      printf("%d\n", qArr[i]);
+    }
+  }
+  // Just For Printing the xArr
+  for (int i = 0; i < xNum; i++) {
+    if (i != (xNum - 1)) {
+      printf("%d, ", xArr[i]);
+    } else {
+      printf("%d\n", xArr[i]);
+    }
+  }
+  // Just For Printing the yArr
+  for (int i = 0; i < yNum; i++) {
+    if (i != (yNum - 1)) {
+      printf("%d, ", yArr[i]);
+    } else {
+      printf("%d\n", yArr[i]);
+    }
+  }
+
+  /*
+  ******Creating QSort Shared Memory*****
+  */
+  int qShmID;
+  key_t qKey;
+  int *qShared;
+  char *qCom = "./qsort";
+  char qcomMax[20];
+  char qcomShaID[20];
+  // "q" = 113 ID to use for qsort shared memory
+  // "a" = 97 ID to use for xArr shared memory
+  // "z" = 122 ID to use for yArr shared memory
+  qKey = ftok("./", 113);
+  qShmID = shmget(qKey, sizeof(int[qNum]), IPC_CREAT | 0666);
+  qShared = (int *)shmat(qShmID, NULL, 0);
+  if (qShared < 0) {
+    printf("shmat Failed for qShared");
+    return 1;
+  }
+  // Loading qShared Array
+  for (int i = 0; i < qNum; i++) {
+    qShared[i] = qArr[i];
+  }
+  sprintf(qcomMax, "%d", qNum);
+  sprintf(qcomShaID, "%d", qShmID);
+  char *qargList[] = {qCom, qcomShaID, qcomMax, NULL};
+
+  /*
+  ******Creating xArr Shared Memory*****
+  */
+  int xShmID;
+  key_t xKey;
+  int *xShared;
+  char *mCom = "./merge";
+  char xcomMax[20];
+  char xcomShaID[20];
+  // "q" = 113 ID to use for qsort shared memory
+  // "a" = 97 ID to use for xArr shared memory
+  // "z" = 122 ID to use for yArr shared memory
+  xKey = ftok("./", 97);
+  xShmID = shmget(xKey, sizeof(int[xNum]), IPC_CREAT | 0666);
+  xShared = (int *)shmat(xShmID, NULL, 0);
+  if (xShared < 0) {
+    printf("shmat Failed for xShared");
+    return 1;
+  }
+  // Loading xShared Array
+  for (int i = 0; i < xNum; i++) {
+    xShared[i] = xArr[i];
+  }
+
+  sprintf(xcomMax, "%d", xNum);
+  sprintf(xcomShaID, "%d", xShmID);
+
+  /*
+  ******Creating yArr Shared Memory*****
+  */
+  int yShmID;
+  key_t yKey;
+  int *yShared;
+  char ycomMax[20];
+  char ycomShaID[20];
+  // "q" = 113 ID to use for qsort shared memory
+  // "a" = 97 ID to use for xArr shared memory
+  // "z" = 122 ID to use for yArr shared memory
+  yKey = ftok("./", 122);
+  yShmID = shmget(yKey, sizeof(int[yNum]), IPC_CREAT | 0666);
+  yShared = (int *)shmat(yShmID, NULL, 0);
+  if (yShared < 0) {
+    printf("shmat Failed for yShared");
+    return 1;
+  }
+  // Loading yShared Array
+  for (int i = 0; i < yNum; i++) {
+    yShared[i] = yArr[i];
+  }
+
+  sprintf(ycomMax, "%d", yNum);
+  sprintf(ycomShaID, "%d", yShmID);
+  char *margList[] = {mCom, xcomShaID, xcomMax, ycomShaID, ycomMax, NULL};
+
+  /*
+  -------------------TODO-------------------
+  Next up is to make an empty array for merge to put all of the numbers in their
+  order Check to see if all programs are getting the correct information
+  */
+  /*
+  ********* ONLY PRINTING SHARED ARRAYS FROM HERE
+  */
+  for (int i = 0; i < 3; i++) {
+    printf("%s\t", qargList[i]);
   }
   printf("\n");
-  fclose(fptr);
+  for (int i = 0; i < 5; i++) {
+    printf("%s\t", margList[i]);
+  }
+  printf("\n");
+
+  // Just For Printing the shared qArray
+  printf("qArr \n");
+  for (int i = 0; i < qNum; i++) {
+    if (i != (qNum - 1)) {
+      printf("%d, ", qShared[i]);
+    } else {
+      printf("%d\n", qShared[i]);
+    }
+  }
+  printf("xArr \n");
+  for (int i = 0; i < xNum; i++) {
+    if (i != (xNum - 1)) {
+      printf("%d, ", xShared[i]);
+    } else {
+      printf("%d\n", xShared[i]);
+    }
+  }
+
+  printf("yArr \n");
+  for (int i = 0; i < yNum; i++) {
+    if (i != (yNum - 1)) {
+      printf("%d, ", yShared[i]);
+    } else {
+      printf("%d\n", yShared[i]);
+    }
+  }
+  sleep(3);
+
+  // char* mArgs[] = {ShmID, qNum, NULL};
+  // execvp();
   return 0;
 }
+
+/*
+  Used by other proccesses to access shared memory
+  int *qShared;
+  qShared = (int *)shmat(ShmID, NULL, 0);
+*/
--- merge.c
+++ merge.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
+#include <stdlib.h>
 
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <sys/types.h>
 /*
 ** Inputs Given by command line from main
 ** ./merge.c xHigh yHigh xArry[] yArr[]
@@ -21,32 +25,59 @@ void findLocation(int xArr[], int yArr[], int xToFind, int ySize,
   printf("%d\n", posHolder);
   fArray[posHolder + xToFind] = xArr[xToFind];
 }
-
-int main() {
-  int array1[] = {-20, -19, -14, -13, -11, -10, -8, -5, -3, 0,
-                  4,   5,   6,   7,   8,   9,   12, 14, 16, 19};
-  int array2[] = {-18, -17, -16, -15, -12, -9, -7, -6, -4, -2, -1,
-                  1,   2,   3,   10,  11,  13, 15, 17, 18, 20};
-
-  int fArr[41];
-
-  for (int i = 0; i < 41; i++) {
-    fArr[i] = -1;
+/*
+  Used by other proccesses to access shared memory
+  int *qShared;
+  qShared = (int *)shmat(ShmID, NULL, 0);
+*/
+int main(int argc, char *argv[]) {
+  /*
+  Input Args given
+  ./merge SharedID maxNum
+  */
+  printf("Printed From merge\n");
+  printf("%d\n", argc);
+  for (int i = 0; i < argc; i++) {
+    printf("%s\n", argv[i]);
   }
+  int ShaID = atoi(argv[1]);
+  int max = atoi(argv[2]);
 
-  // for(int i=0; i< 5; i++){
+  int *sharingA;
+  sharingA = (int *)shmat(ShaID, NULL, 0);
 
+  for (int i = 0; i < max; i++) {
+    if (i != (max - 1)) {
+      printf("%d, ", sharingA[i]);
+    } else {
+      printf("%d\n", sharingA[i]);
+    }
+  }
+  return 0;
+  // int array1[] = {-20, -19, -14, -13, -11, -10, -8, -5, -3, 0,
+  //                 4,   5,   6,   7,   8,   9,   12, 14, 16, 19};
+  // int array2[] = {-18, -17, -16, -15, -12, -9, -7, -6, -4, -2, -1,
+  //                 1,   2,   3,   10,  11,  13, 15, 17, 18, 20};
+
+  // int fArr[41];
+
+  // for (int i = 0; i < 41; i++) {
+  //   fArr[i] = -1;
   // }
-  for (int i = 0; i < 20; i++) {
 
-    findLocation(array1, array2, i, 21, fArr);
-  }
-  for (int i = 0; i < 21; i++) {
+  // // for(int i=0; i< 5; i++){
 
-    findLocation(array2, array1, i, 21, fArr);
-  }
+  // // }
+  // for (int i = 0; i < 20; i++) {
 
-  for (int i = 0; i < 41; i++) {
-    printf("%d, ", fArr[i]);
-  }
+  //   findLocation(array1, array2, i, 21, fArr);
+  // }
+  // for (int i = 0; i < 21; i++) {
+
+  //   findLocation(array2, array1, i, 21, fArr);
+  // }
+
+  // for (int i = 0; i < 41; i++) {
+  //   printf("%d, ", fArr[i]);
+  // }
 }
